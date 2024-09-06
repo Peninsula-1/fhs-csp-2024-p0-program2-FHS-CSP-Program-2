6efb37977b8ad31c7f7ba1d85481ce7a381ef99c
Peninsula-1 <kirannewey.folsom@gmail.com>
2024-09-06 14:37:07 +0000

    Done

--- Program2.py
+++ Program2.py
@@ -50,9 +50,9 @@
 #Please write a script that: 
 # - Asks for the user's name and then prints it twice, on two consecutive lines.
 
-
-
-
+name = input('What is your name? ')
+print(name)
+print(name)
 
 ## Problem 2 ##
 #Please write a script that: 
@@ -60,6 +60,8 @@
 # - Prints it out twice on a single line so that there is an exclamation mark at the beginning of the line, 
 # - another between the two names and a third one at the end of the line.
 
+name = input('What is your name? ')
+print(f'!{name}!{name}!')
 
 ## Problem 3 ##
 #Please write a script that: 
@@ -71,11 +73,26 @@
 #   - Street address: 91 Station Road
 #   - City and postal code: Folsom CA, 95630
 
+fname = input('What is your FIRST name? ')
+lname = input('What is your LAST name? ')
+sad = input('What street is your house on? ')
+cap = input('What city do you live in (include postal code)? ')
+
+print(f'First name: {fname}')
+print(f'Last name: {lname}')
+print(f'Street address: {sad}')
+print(f'City and Postal Code: {cap}')
+
 ## Problem 4 ##
 #Please write a script that: 
 # - Asks for 3 words 
 # - Puts the words together with dashes and prints that out
 
+wa = input('Type a word: ')
+wb = input('Type another word: ')
+wc = input('Type a third word: ')
+
+print(f'{wa}-{wb}-{wc}')
 
 ## Problem 5 ##
 #Please write a script that: 
@@ -88,3 +105,7 @@
 #Mary is a valiant knight, born in the year 1572. 
 #One morning Mary woke up to an awful racket: a dragon was approaching the village. 
 #Only Mary could save the village's residents.
+
+name = input ('Type in a name:')
+year = input ('Type in a year: ')
+print(f'One day in the year {year}, {name} decided to go on a walk. \nOn their walk, they found a mysterious orb. \n{name} touched the orb and was transported back in time, thousands of years before {year}.')
